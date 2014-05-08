c97394bc256fb1e5837b8e96cbd78c0e2387bdfc
jacobfreise <jacob.freise@student.hpi.uni-potsdam.de>
2014-05-08 17:32:22 +0200

    Optimieren der Zusatzaufgabe

--- Uebung2/src/assignment2/DogTraining.java
+++ Uebung2/src/assignment2/DogTraining.java
@@ -1,22 +1,44 @@
 package assignment2;
 
+/* 
+ * 1) Der Algorithmus verwendet Techniken von Countingsort. Es wird zunächst ein Hilfsarray erzeugt, das die niedrigsten Zeiten für die Werte von
+ * b speichert. 
+ * 2) Der Hilfsarray wird dann nach validen Kombinationen für k durchsucht, hierbei muss man nur die Indizes 1 ... k/2 - (k+1) % 2 durchsuchen, weil sich 
+ * die restlichen Kombinationen durch das Kommutativgesetz der Addition ergeben. Man muss allerdings den Fall ausschließen, dass i und k-i gleich sind,
+ * deswegen der Modulo Teil.
+ * Es darf nur result überschrieben werden, wenn i und k-i in b vorkommen, was gleichbedeutend ist mit timePerNumber[i] und timePerNumber[k-i] sind
+ * ungleich -1
+ * Die Variable result hält das momentan niedrigste Ergebnis und wird mit -1 initialisiert, weil dies der Standardwert der Ausgabe ist,
+ * wenn keine gültige Kombination gefunden wird.
+ * 
+ */
+
 public class DogTraining {
 	public static int secondsToWait(int k, int[] b) {
-		int[] timePerNumber = new int[findMax(b) + 1];
+		//1)
+		int[] timePerNumber = new int[k];
 		
+		//Standardwert -1
 		for (int i = 0; i < timePerNumber.length; i++){
 			timePerNumber[i] = -1;
 		}
 		
+		//Befüllen des Hilfsarrays mit den Zeiten
 		for (int i = 0; i < b.length; i++){
-			if ( (timePerNumber[b[i]] == -1) || (timePerNumber[b[i]] > findMinTime(i, b.length)) ){
-				timePerNumber[b[i]] = findMinTime(i, b.length);
+			if (b[i] < k){
+				if ( (timePerNumber[b[i]] == -1) || (timePerNumber[b[i]] > findMinTime(i, b.length)) ){
+					timePerNumber[b[i]] = findMinTime(i, b.length);
+				}
 			}
 		}
 		
+		// Ende 1)
+		
+		//2)
 		int result = -1;
 		
-		for (int i = 0; i < k/2 - ((k+1) % 2); i++){
+		for (int i = 1; i < k/2 - ((k+1) % 2); i++){
+			//valide Kombination vorhanden? => überschreibe result mit der niedrigeren Zeit
 			if ((timePerNumber[i] != -1) && (timePerNumber[k-i] != -1)){
 				if (result == -1){
 					result = Math.max(timePerNumber[i], timePerNumber[k-i]);
@@ -37,16 +59,4 @@ public class DogTraining {
 		
 		return length - index;
 	}
-	
-	private static int findMax(int[] a){
-		int result = 1;
-		for (int i = 0; i < a.length; i++){
-			if (a[i] > result){
-				result = a[i];
-			}
-		}
-		return result;
-	}
-	
-	
 }

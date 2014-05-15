d9f7d7c9d3be297a352ea57c5be927794d497bdd
Jacob Freise <jacob.freise@student.hpi.uni-potsdam.de>
2014-05-15 11:56:16 +0200

    Uebung3

---
+++ Uebung3/test/assignment3/SquareMatrixTest.java
@@ -0,0 +1,23 @@
+package assignment3;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class SquareMatrixTest {
+	@Test
+	public void testMin() {
+	}
+	
+	@Test
+	public void testMax() {
+	}
+	
+	@Test
+	public void testMultiplyWithScalar() {
+	}
+	
+	@Test
+	public void testMultiplyWithMatrix() {
+	}
+	
+}

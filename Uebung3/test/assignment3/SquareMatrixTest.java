3cca660bbfff08c6d18c1e5a1d7912a8070b5124
Jacob Freise <jacob.freise@student.hpi.uni-potsdam.de>
2014-05-22 12:34:04 +0200

    Aufgabe 1 großteils bearbeitet
    es fehlen Tests für Methode multiply(Matrix)

--- Uebung3/test/assignment3/SquareMatrixTest.java
+++ Uebung3/test/assignment3/SquareMatrixTest.java
@@ -1,19 +1,63 @@
 package assignment3;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.internal.runners.statements.ExpectException;
+
+import assignment3.SquareMatrix.InitStrategy;
 
 public class SquareMatrixTest {
+	
+	SquareMatrix matrix1;
+	SquareMatrix matrix2;
+	
+	@Before
+	public void setup(){
+		matrix1 = new SquareMatrix(4, InitStrategy.Plus);
+		matrix2 = new SquareMatrix(4, InitStrategy.Minus);
+		
+	}
+	
 	@Test
 	public void testMin() {
+		assertEquals(0d, matrix1.min(), 0);
+		assertEquals(-3d, matrix2.min(), 0);
+		for (int i = 0; i < matrix1.size(); i++) {
+			for (int j = 0; j < matrix1.size(); j++){
+				matrix1.set(i, j, Double.NaN);
+			}
+		}
+		assertEquals(Double.POSITIVE_INFINITY, matrix1.min(), 0);
 	}
 	
 	@Test
 	public void testMax() {
+		assertEquals(6d, matrix1.max(), 0);
+		assertEquals(3d, matrix2.max(), 0);
+		for (int i = 0; i < matrix1.size(); i++) {
+			for (int j = 0; j < matrix1.size(); j++){
+				matrix1.set(i, j, Double.NaN);
+			}
+		}
+		assertEquals(Double.NEGATIVE_INFINITY, matrix1.max(), 0);
 	}
 	
 	@Test
 	public void testMultiplyWithScalar() {
+		boolean test = true;
+		SquareMatrix testmatrix = matrix1.multiply(3.0);
+		SquareMatrix result = new SquareMatrix(4, InitStrategy.Plus);
+		for (int i = 0; i < result.size(); i++) {
+			for (int j = 0; j < result.size(); j++){
+				result.set(i, j, result.get(i, j)*3.0);
+				if (result.get(i, j) != testmatrix.get(i, j)){
+					test = false;
+				}
+			}
+		}
+		assertTrue(test);
 	}
 	
 	@Test

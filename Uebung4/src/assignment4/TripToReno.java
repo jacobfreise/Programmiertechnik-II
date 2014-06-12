dd6e55c3b2ef5045546aaa7b47b5964ad47129e5
starfmvfb <sebastian@Bastis-MacBook.local>
2014-06-12 14:02:07 +0200

    1.aufgabe
    funktioniert bisher nur mit der IntegerList - int[]  Umwandlung aus nem
    Forum

--- Uebung4/src/assignment4/TripToReno.java
+++ Uebung4/src/assignment4/TripToReno.java
@@ -1,14 +1,59 @@
 package assignment4;
 
+import java.util.ArrayList;
+
 public class TripToReno {
 
 	public static int[] getStops(int d, int z, int[] t) {
-		//TODO
-		return null;
-	}
+		ArrayList<Integer> result = new ArrayList<Integer>();
+        int capacity = d;
+        int waycount = 0;
+        int count;
+        for(count=0;count<t.length-1;count++){
+            d=d-t[count];
+            if(d-t[count+1]<0){
+                result.add(count);
+                waycount+= capacity-d;
+                d= capacity;
+            }
+        }
+        count++;
+        if((z-waycount)>d){
+            result.add(count);
+        }
+        
+        //int[] result_final = new int[result.size()];
+        //result_final= result.toArray();
+        return ListToIntArray(result);
+        //return result.toArray();
+    }
 
+	public static int[] ListToIntArray(ArrayList<Integer> list) {
+        int[] array = new int[list.size()];
+        
+        for (int i = 0; i < array.length; i++)
+            array[i] = list.get(i).intValue();
+        
+        return array;
+    }
+	
 	public static int timeCompare(int kmhProf, int kmhBike, int d, int z, int[] t) {
-		//TODO
-		return 0;
+        int[] stops = getStops(d,z,t);
+        float timeProf = (float) z/kmhProf;
+        float timeBike = (float) z/kmhBike;
+		if(timeProf*60+stops.length*15 > timeBike*60){
+            return -1;
+        }
+        else{
+            if(timeProf*60+stops.length*15 < timeBike*60){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
 	}
+    
+    
+    
 }

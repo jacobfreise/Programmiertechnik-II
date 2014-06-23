9d597ff194b9fea802ca09abd1a20ed87bbf382a
Jacob Freise <jacob.freise@student.hpi.uni-potsdam.de>
2014-06-23 13:27:35 +0200

    array sequence Methoden erstellt

---
+++ Uebung5/src/assignment5/ArraySequence.java
@@ -0,0 +1,71 @@
+package assignment5;
+
+import java.util.Iterator;
+
+import assignment5.adt.List;
+import assignment5.adt.Queue;
+import assignment5.adt.Stack;
+
+public class ArraySequence<T extends Comparable<T>> implements List<T>, Stack<T>, Queue<T> {
+
+	@Override
+	public int length() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void enqueue(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T dequeue() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void push(T element) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T pop() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public T change(int index, T item) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void insert(int index, T item) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Iterator<T> traverse() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

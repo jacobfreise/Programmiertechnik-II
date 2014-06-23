efa515940a373bdd7f57fb276567122bdba7169e
Jonathan <Jonathan@Hasenburg.de>
2014-06-23 15:22:33 +0200

    Started ArraySequenze

--- Uebung5/src/assignment5/ArraySequence.java
+++ Uebung5/src/assignment5/ArraySequence.java
@@ -8,22 +8,52 @@ import assignment5.adt.Stack;
 
 public class ArraySequence<T extends Comparable<T>> implements List<T>, Stack<T>, Queue<T> {
 
+	private T storage[];
+	private int length;
+	private int nextFirstItem;
+	private int nextLastItem;
+	private static int size = 25;
+
+	@SuppressWarnings("unchecked")
+	public ArraySequence() {
+		storage = (T[]) new Object[size];
+		length = 0;
+		nextFirstItem = storage.length / 2;
+		nextLastItem = storage.length / 2 + 1;
+	}
+
 	@Override
 	public int length() {
-		// TODO Auto-generated method stub
-		return 0;
+		return length;
 	}
 
 	@Override
 	public void enqueue(T element) {
-		// TODO Auto-generated method stub
-		
+		if (nextLastItem >= storage.length -1 ) { //reached end of array
+			storage[nextLastItem] = element;
+			nextLastItem++;
+			length++;
+		} else {
+			//TODO resize storage (storage.size += size);
+		}
 	}
 
 	@Override
 	public T dequeue() {
-		// TODO Auto-generated method stub
-		return null;
+		T item = null;
+		if (length != 0) {
+			int position = nextFirstItem + 1;
+			item = storage[position];
+			storage[position] = null;
+			length--;
+			if (length == 0) { //reset start positions
+				nextFirstItem = storage.length / 2;
+				nextLastItem = storage.length / 2 + 1;
+			}
+		} else {
+			//TODO throw exception
+		}
+		return item;
 	}
 
 	@Override
